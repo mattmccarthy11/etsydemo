59784a26b9ee405b452ebbcb24bed95a5b498fd3
Matthew McCarthy <mattmccarthy11@gmail.com>
2015-06-03 23:52:10 -0500

    Added routes to route to listings, created database with generate scaffold, the migrated the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :listings
   get 'pages/about'
 
   get 'pages/contact'
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'pages#index'
+   root 'listings#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
